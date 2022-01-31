7089cb6ce28a31344fbab042197f386b138a4543
Thomas Boittin <tboittin@gmail.com>
2022-01-31 04:32:36 -0500

    change bash file to java file

--- main.java
+++ main.java
@@ -1,10 +1,5 @@
-mkdir -p /tmp/ajc
-rm -rf /tmp/ajc/*
-echo '
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World !") ;
     }
 }
-' > /tmp/ajc/Main.java
-javac /tmp/ajc/Main.java
